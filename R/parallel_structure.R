84407928c4023b001df978568e12f25e249eff03
pickletime <22991056+pickletime@users.noreply.github.com>
2018-05-22 16:50:44 -0500

    remove numboxes
    hopefully this fixes it

--- R/parallel_structure.R
+++ R/parallel_structure.R
@@ -24,7 +24,7 @@ function(joblist=NULL,n_cpu=NULL,structure_path=Mac_path,infile=NULL,outpath=NUL
 		recessivealleles=0,phased=0,extracol=0,missing=-9,ploidy=2,noadmix=0,linkage=0,usepopinfo=0,locprior=0,
 		inferalpha=1,alpha=1.0,popalphas=0,unifprioralpha=1,alphamax=10.0,alphapropsd=0.025,freqscorr=1,onefst=0,
 		fpriormean=0.01,fpriorsd=0.05,inferlambda=0,lambda=1.0,computeprob=1,pfromflagonly=0,ancestdist=0,
-		startatpopinfo=0,metrofreq=10,updatefreq=1,printqhat=0,revert_convert=0,randomize=1,gensback=2, migrprior=0.05, ancestpint = 0.975, numboxes = 1000){
+		startatpopinfo=0,metrofreq=10,updatefreq=1,printqhat=0,revert_convert=0,randomize=1,gensback=2, migrprior=0.05, ancestpint = 0.975){
 			
 	
 		require(parallel)
@@ -35,7 +35,7 @@ function(joblist=NULL,n_cpu=NULL,structure_path=Mac_path,infile=NULL,outpath=NUL
 				 noadmix=noadmix,linkage=linkage,usepopinfo=usepopinfo,locprior=locprior,inferalpha=inferalpha,alpha=alpha,popalphas=popalphas,
 				 unifprioralpha=unifprioralpha,alphamax=alphamax,alphapropsd=alphapropsd,freqscorr=freqscorr,onefst=onefst,fpriormean=fpriormean,fpriorsd=fpriorsd,
 				 inferlambda=inferlambda,lambda=lambda,computeprob=computeprob,pfromflagonly=pfromflagonly,ancestdist=ancestdist,startatpopinfo=startatpopinfo,
-				 metrofreq=metrofreq,updatefreq=updatefreq,printqhat=printqhat,randomize=randomize,gensback=gensback,migrprior=migrprior, ancestpint = ancestpint, numboxes = numboxes)
+				 metrofreq=metrofreq,updatefreq=updatefreq,printqhat=printqhat,randomize=randomize,gensback=gensback,migrprior=migrprior, ancestpint = ancestpint,)
 
 	mes=paste('starting work at ',Sys.time(),sep='')
 	print(mes)
