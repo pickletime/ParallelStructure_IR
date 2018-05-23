c88eaccbe7a4dc17989f13533856861fca84010e
pickletime <22991056+pickletime@users.noreply.github.com>
2018-05-23 11:22:03 -0500

    changing default values
    fpriormean and fpriorsd are now in line with structure proper

--- R/parallel_structure.R
+++ R/parallel_structure.R
@@ -23,7 +23,7 @@ function(joblist=NULL,n_cpu=NULL,structure_path=Mac_path,infile=NULL,outpath=NUL
 		plot_output=1,label=1,popdata=1,popflag=0,locdata=0,phenotypes=0,markernames=0,mapdist=0,onerowperind=0,phaseinfo=0,
 		recessivealleles=0,phased=0,extracol=0,missing=-9,ploidy=2,noadmix=0,linkage=0,usepopinfo=0,locprior=0,
 		inferalpha=1,alpha=1.0,popalphas=0,unifprioralpha=1,alphamax=10.0,alphapropsd=0.025,freqscorr=1,onefst=0,
-		fpriormean=0.01,fpriorsd=0.05,inferlambda=0,lambda=1.0,computeprob=1,pfromflagonly=0,ancestdist=0,
+		fpriormean=0.1,fpriorsd=0.1,inferlambda=0,lambda=1.0,computeprob=1,pfromflagonly=0,ancestdist=0,
 		startatpopinfo=0,metrofreq=10,updatefreq=1,printqhat=0,revert_convert=0,randomize=1,gensback=2, migrprior=0.05, ancestpint = 0.975){
 			
 	
